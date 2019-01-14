09c6499e66de571de470c62c69a5e36cfbe974d2
Michael Irwin <mikesir87@gmail.com>
2019-01-14 14:17:19 -0500

    Added unit test for eventHandler

---
+++ task-satisfier/spec/eventHandler.spec.js
@@ -0,0 +1,59 @@
+const eventHandler = require('../src/eventHandler');
+
+describe('eventHandler', () => {
+    let event, satisfier, responseSender;
+
+    beforeEach(() => {
+        event = {
+            stack: { id: 'master' },
+            task: {
+                name: 'sample-task',
+                executor: 'lambda',
+                config: {
+                    name: 'sample-task',
+                },
+            },
+        };
+
+        satisfier = jasmine
+            .createSpy('satisfier')
+            .and.returnValue(Promise.resolve());
+
+        responseSender = {
+            sendSuccess: jasmine
+                .createSpy('responseSender.sendSuccess')
+                .and.returnValue(Promise.resolve()),
+            sendError: jasmine
+                .createSpy('responseSender.sendError')
+                .and.returnValue(Promise.resolve()),
+        };
+    });
+
+    it('sends success when satisfying completes successfully', async () => {
+        await eventHandler(event, satisfier, responseSender);
+
+        expect(satisfier).toHaveBeenCalledWith(event.stack, event.task);
+        expect(responseSender.sendSuccess).toHaveBeenCalledWith(
+            event.stack.id,
+            event.task,
+        );
+        expect(responseSender.sendError).not.toHaveBeenCalled();
+    });
+
+    it('sends error when satisfying fails via Promise rejection', async () => {
+        const errorMessage = 'An ERROR has occurred';
+        satisfier = jasmine
+            .createSpy('satisfier')
+            .and.returnValue(Promise.reject(new Error(errorMessage)));
+
+        await eventHandler(event, satisfier, responseSender);
+
+        expect(satisfier).toHaveBeenCalledWith(event.stack, event.task);
+        expect(responseSender.sendSuccess).not.toHaveBeenCalled();
+        expect(responseSender.sendError).toHaveBeenCalledWith(
+            event.stack.id,
+            event.task,
+            errorMessage,
+        );
+    });
+});
